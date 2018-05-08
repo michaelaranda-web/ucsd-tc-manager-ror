acf6ff0d90d27cc11a46c3c507ad7a735fc04e55
Michael Aranda <michaelaranda0.web@gmail.com>
2018-05-08 18:09:16 +0000

    Adds Devise for authentication. Hopefully sets up mailing for app hosted on Heroku.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,9 +16,6 @@ config.webpacker.check_yarn_integrity = true
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
