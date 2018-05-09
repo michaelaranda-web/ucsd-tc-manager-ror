a887d16c6ae951a22ae3a7b4d21598680c1f3d26
Michael Aranda <michaelaranda0.web@gmail.com>
2018-05-09 00:10:21 +0000

    Adds React, in the process of creating the attendee selector component for the create event page.

---
+++ app/javascript/components/events/new/attendeesSelector.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class AttendeesSelector extends React.Component {
+  render() {
+    return (
+      <div>React is working!</div>
+    )
+  }
+}
+
+export default AttendeesSelector;
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,1 +1,10 @@
-console.log("Hello from index.jsx!");
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AttendeesSelector from '../components/events/new/attendeesSelector.jsx';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <AttendeesSelector />, 
+    document.querySelector('.attendees-selector'));
+})
