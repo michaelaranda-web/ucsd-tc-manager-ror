647dc7c007f9c0c3c874953b6ff12f75490cfcc8
Michael Aranda <michaelaranda0.web@gmail.com>
2018-05-11 22:06:52 +0000

    Defines destroy dependencies to Member and Event models for EventAttendedByMember model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ActiveRecord::Base
   validates :name, :volunteer_hours, :driving_distance, presence: true
   
   belongs_to :event_type
-  has_many :event_attended_by_members
+  has_many :event_attended_by_members, dependent: :destroy
   has_many :attendees, through: :event_attended_by_members, source: :member
 end
